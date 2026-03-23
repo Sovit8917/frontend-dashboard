4be98cb109f14bceed405f578c6ab7edcf8697e1
Sovit8917 <lenkasovitsarbeswar@gmail.com>
2026-03-23 14:01:54 +0530

    redirect problem

--- components/AuthRedirect.tsx
+++ components/AuthRedirect.tsx
@@ -1,15 +1,25 @@
 "use client";
-import { redirect, usePathname } from "next/navigation";
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import { buildLoginRedirect } from "@/lib/auth-redirect";
 
 const AuthRedirect = () => {
   const pathname = usePathname();
+  const router = useRouter();
 
-  return redirect(
-    pathname === "/login" || pathname === "/"
-      ? "/login"
-      : buildLoginRedirect(pathname),
-  );
+  useEffect(() => {
+    const login = "/login";
+    const homePage = "/";
+
+    const target =
+      pathname === login || pathname === homePage
+        ? login
+        : buildLoginRedirect(pathname);
+
+    router.replace(target);
+  }, [pathname, router]);
+
+  return null;
 };
 
 export default AuthRedirect;
